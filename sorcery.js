8913b162b70e8bd02891d79b0d2d29f434c45b46
vmorreel <dawin.vmorreel@gmail.com>
2017-01-24 22:19:06 +0100

    Introducing stories (Y.V & Eyes) + Adding main events of the story

--- sorcery.js
+++ sorcery.js
@@ -1,107 +1,117 @@
 $( function() {
-	var buttons = $(".section button");
-	var status = $("#status");
+    var buttons = $(".section button");
+    var status = $("#status");
+    status.hide();
     startGame();
 
-	buttons.click( function() {
+    buttons.click( function() {
         var go = $(this).attr('go');
-        if(go == "intro"){
+        if(go == "intro")
+        {
             startGame();
-        }else{
+        }
+        else{
             gotoSection(go);    
         }
-          
-        
     });
-	
-	
-	
-	function gotoSection(key) {
+
+
+    function gotoSection(key) {
         $('.section').hide();
         var section = $('.section#'+key);
-		section.show();
+        section.show();
         var child = section.children('action');
         var actionName = child.attr('name');
         var actionLose = child.attr('lose');
-            
-        
+
+
         if(actionName == "hit"){
-            if(actionLose != undefined){
+            if(actionLose != undefined)
+            {
                 setLife(actionLose);
-            }else{
+            }
+            else
+            {
                 loseOneLife();
             }
         }
+
         
+
         if(section.children('a').html() != undefined){
-            
+
             var button = section.children('button');
             button.css({"display":"none"});
             var chance = 0;
-            
+
             section.children('a').click(function(){
                 var reponse = $('input#question_'+key).val();
                 chance ++;
-                
+
                 if(reponse == "reponse"){
                     button.css({"display":"block"});
                     $('a#lose').remove();
-                }else{
+                }
+                else
+                {
                     if(chance == 1)
                         $(this).after('<p><a id="lose">Faire apparaître le bouton en échange d\'une vie.</a></p>')
-                    
                 }
-                
-                
-                $('a#lose').click(function(){
+
+
+                $('a#lose').click(function()
+                {
                     button.css({"display":"block"});
                     loseOneLife();
                     $(this).remove();
                 });
             });
         }
-        
-	}
-	
-	function getLife() {
-		var life = status.find('span.value').html();
+
+    }
+
+    function getLife() {
+        var life = status.find('span.value').html();
         return life;
-  
-	}
-    
-	function setLife(v) {
-		var life = getLife();
+
+    }
+
+    function setLife(v) {
+        var life = getLife();
         life = life-v;
         status.find('span.value').html(life);
-        if(life <= 0){
-             status.find('span.value').html("0");
+        if(life <= 0)
+        {
+            status.find('span.value').html("0");
             endGame();
-        }else{
-             status.find('span.value').html(life);
         }
-	}
-	
-	function loseOneLife() {
-		var life = getLife();
+        else{
+            status.find('span.value').html(life);
+        }
+    }
+
+    function loseOneLife() {
+        var life = getLife();
         life = life-1;
-        if(life <= 0){
-             status.find('span.value').html("0");
+        if(life <= 0)
+        {
+            status.find('span.value').html("0");
             endGame();
-        }else{
-             status.find('span.value').html(life);
         }
-            
-	}
-	
-	function startGame() {
-		$('.section').hide();
+        else{
+            status.find('span.value').html(life);
+        }
+
+    }
+
+    function startGame() {
+        $('.section').hide();
         $('.section#intro').show();
         status.find('span.value').html("3");
-	}
-	
-	function endGame() {
-		$('.section').hide();
+    }
+
+    function endGame() {
+        $('.section').hide();
         $('.section#death').show();        
-	}
-	
-} );
+    }
+});
